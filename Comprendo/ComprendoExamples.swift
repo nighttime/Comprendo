8f7c7c858ade40caec58302ec84386069c70b37f
nighttime <softwareatnight@gmail.com>
2014-10-20 15:17:45 -0400

    Updated Examples

--- Comprendo/ComprendoExamples.swift
+++ Comprendo/ComprendoExamples.swift
@@ -14,6 +14,8 @@ struct Tests {
     func doTests() {
         let nums = [1, 2, 3, 4]
         
+        // Each of the below prints [2, 3, 4, 5]
+        
         let nums1 = nums |> {$0 + 1}
         println(nums1)
         
@@ -26,17 +28,22 @@ struct Tests {
         let nums4 = enumerate(nums) |> {$0.1 + 1}
         println(nums4)
         
+        // Conditionally print only when index is even
+        // Prints [3, 5]
+        
         let nums5 = enumerate(nums) | {$0.0 % 2 == 0} |> {$0.1 + 1}
         println(nums5)
+        
+        // Prints [0:2, 1:3, 2:4, 3:5]
         
         let nums6 = enumerate(nums) |>> {$0.1} => {$0.1 + 1}
         println(nums6)
         
+        // Conditionally print only when num is even
+        // Prints [1:3, 3:5]
+        
         let nums7 = enumerate(nums) | {$0.1 % 2 == 0} |>> {$0.0} => {$0.1 + 1}
         println(nums7)
-        
-        let nums8 = (1..<5) | {$0 % 2 == 0} |> {$0 + 1}
-        println(nums8)
     }
     
 }
